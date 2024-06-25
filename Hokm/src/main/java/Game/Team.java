e773ba2d8f19d039715ef6fd911e176b1583d37d
The Navid <thenavid2005@gmail.com>
2024-06-25 17:50:31 +0330

    Team class: remove static type, create constructor, create hakem method to randomly select the ruler from among the players.

--- Hokm/src/main/java/Game/Team.java
+++ Hokm/src/main/java/Game/Team.java
@@ -1,23 +1,53 @@
 package Game;
 
-public class Team extends Player {
-        private static Player player1 ;
-        private static Player player2 ;
-        public Team () {
-            super();
-            this.player1 = new Player("","");
-            this.player2 = new Player("","");
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Team {
+        private Player player1 ;
+        private Player player2 ;
+        private Player player3 ;
+        private Player player4 ;
+        public Team (Player player1, Player player2, Player player3, Player player4) {
+            this.player1 = player1;
+            this.player2 = player2;
+            this.player3 = player3;
+            this.player4 = player4;
         }
-    public static Player getPlayer1() {
-        return player1;
-    }
-    public static Player getPlayer2() {
-        return player2;
-    }
-    public static void setPlayer1(Player player1) {
-        Team.player1 = player1;
-    }
-    public static void setPlayer2(Player player2) {
-        Team.player2 = player2;
-    }
-}
+    
+        public void Hakem()
+        {
+            Player[] players = {player1, player2, player3, player4};
+            Random random = new Random();
+            int randomIndex = random.nextInt(4);
+            Player hakem = players[randomIndex];
+            System.out.println("Hakem is:" + hakem.getName());
+        }
+        public Player getPlayer1() {
+            return player1;
+        }
+        public void setPlayer1(Player player1) {
+            this.player1 = player1;
+        }
+        public Player getPlayer2() {
+            return player2;
+        }
+        public void setPlayer2(Player player2) {
+            this.player2 = player2;
+        }
+        public Player getPlayer3() {
+            return player3;
+        }
+        public void setPlayer3(Player player3) {
+            this.player3 = player3;
+        }
+        public Player getPlayer4() {
+            return player4;
+        }
+        public void setPlayer4(Player player4) {
+            this.player4 = player4;
+        }
+        
+        }
+
+
