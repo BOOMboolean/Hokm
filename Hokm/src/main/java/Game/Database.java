2993a6cf2077eff17ef5dcd284fcdffcdd4bf738
mmzonoz@gmail.com <mmzonoz@gmail.com>
2024-06-28 13:45:37 +0430

    basics of database added

---
+++ Hokm/src/main/java/Game/Database.java
@@ -0,0 +1,32 @@
+package Game;
+
+import java.sql.*;
+
+public class Database {
+    public void addRow(String roundWinner) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hokm", "root", "");
+            PreparedStatement pst = conn.prepareStatement("INSERT INTO score VALUES(default,?)"); //Round is automatically incremented by database
+            pst.setString(1, roundWinner);
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void printData() {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hokm", "root", "");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM score");
+
+            System.out.println("  Round  |   Winner");
+            while (rs.next()) {
+                System.out.print("    " + rs.getInt("Round") + "    |");
+                System.out.println("   " + rs.getString("Winner"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
